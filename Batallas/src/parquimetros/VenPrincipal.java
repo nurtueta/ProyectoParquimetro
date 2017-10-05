c99a01321fd9ad489693cca904b62186bfaf469f
Nicolas Landau <ptcnico@hotmail.com>
2017-10-05 19:13:57 -0300

    crear clase

---
+++ Batallas/src/parquimetros/VenPrincipal.java
@@ -0,0 +1,43 @@
+package parquimetros;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class VenPrincipal {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VenPrincipal window = new VenPrincipal();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VenPrincipal() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
