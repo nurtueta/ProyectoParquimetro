9ddc94b641a5ee3c0dbe64431304dcc616f792cf
BatraChin <guillef92@gmail.com>
2017-10-08 00:39:21 -0300

    Agregado de UI de inicio y pwd

--- Batallas/src/parquimetros/VenPrincipal.java
+++ Batallas/src/parquimetros/VenPrincipal.java
@@ -3,10 +3,15 @@ package parquimetros;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class VenPrincipal {
 
 	private JFrame frame;
+	private JTextField txtUsuario;
+	private JPasswordField pwdBox;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,23 @@ public class VenPrincipal {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(261, 109, 89, 23);
+		frame.getContentPane().add(btnLogin);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setText("Usuario");
+		txtUsuario.setBounds(74, 84, 86, 20);
+		frame.getContentPane().add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		pwdBox = new JPasswordField();
+		pwdBox.setEchoChar('*');
+		pwdBox.setToolTipText("Contrase\u00F1a");
+		pwdBox.setBounds(74, 133, 86, 20);
+		frame.getContentPane().add(pwdBox);
+		pwdBox.setColumns(10);
 	}
-
 }
